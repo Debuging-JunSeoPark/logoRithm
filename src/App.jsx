636cf48bda66f13b25b935083f12aea13559e6cc
Debuging-JunSeoPark <pjseo1313@daum.net>
2025-11-22 17:25:08 +0900

    Initialize Vite + React + Tailwind v4 project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Tailwind 정상 작동!</h1>
+    </div>
+  );
+}
+
+export default App;
+

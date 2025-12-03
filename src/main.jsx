2bc922612cb662ba5e74f22ddb03df1edb086551
Debuging-JunSeoPark <pjseo1313@daum.net>
2025-12-03 15:18:18 +0900

    hotfix: 서비스 워커 등록

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,16 @@ createRoot(document.getElementById('root')).render(
     <App />
  // </StrictMode>,
 )
+
+if ('serviceWorker' in navigator && import.meta.env.PROD) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/pwabuilder-sw.js')
+      .then(() => {
+        console.info('Service worker registered')
+      })
+      .catch((error) => {
+        console.error('Service worker registration failed', error)
+      })
+  })
+}
